2c4723e690e1d47c1ff060d098fb407d13d611e2
liukan <liukan2007@126.com>
2018-12-28 17:48:52 +0800

    update mysql connect

--- db.js
+++ db.js
@@ -1,5 +1,37 @@
 const mysql = require('mysql');
 let db = {};
 db.query = function sqlback(sqllan, fn) {
-  const
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'mydb',
+        port: 3306
+    });
+    connection.connect(function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+    const sql = sqllan;
+    if (!sql) {
+        return;
+    }
+    connection.query(sql, function (err, rows, fields) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        fn(rows);
+    });
+    connection.end(function (err) {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            console.log('连接关闭');
+        }
+    })
+
 }
