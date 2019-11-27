095fc2afbffa550b6bcff5d5bbd97a9cbfc3825f
liukan <445469129@qq.com>
2019-11-27 21:27:45 +0800

    update

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+/**
+ * user bean
+ * @constructor
+ */
+var UserInfo = function() {
+    this.id = '';
+    this.userId = '';
+    this.userName = '';
+    this.gender = '';
+    this.account = '';
+    this.companyName = '';
+    this.companyType = '';
+    this.companyId = '';
+    this.linkmanAddress = '';
+    this.linkmanPhone = '';
+    this.createUserName = '';
+    this.createTime = '';
+    this.modifyTime = '';
+    this.mobile = '12341234123';
+    this.email = '';
+    this.iconUrl = '';
+    this.headImgUrl = '';
+    this.roleName = '';
+};
+
+module.exports = UserInfo;
